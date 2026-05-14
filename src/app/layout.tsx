0e1436ad7ae6f09f08b28cf54891cc7412c099b8
RVicky172 <rohillavicky172@gmail.com>
2026-05-14 20:46:48 +0530

    Update app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
-import { Sidebar } from "@/components/Sidebar";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import { ResizableShell } from "@/components/ResizableShell";
 
